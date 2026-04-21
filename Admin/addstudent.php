8b8fd5e62b9ea3092a2791ced384cc7147d24858
Darrshan Ganesan <darrshan987@gmail.com>
2026-04-21 23:33:20 +0800

    addstudent.php

--- Admin/addstudent.php
+++ Admin/addstudent.php
@@ -1,13 +1,13 @@
 x<?php
-// 1. Connect to database
+
 $conn = mysqli_connect("localhost", "root", "", "mathquest");
 
-// 2. Check connection
+
 if (!$conn) {
     die("Connection failed");
 }
 
-// 3. SQL to create table
+
 $sql = "CREATE TABLE students (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
@@ -15,13 +15,13 @@ $sql = "CREATE TABLE students (
     point INT DEFAULT 0
 )";
 
-// 4. Run the SQL
+
 if (mysqli_query($conn, $sql)) {
     echo "Students table created!";
 } else {
     echo "Error creating table";
 }
 
-// 5. Close connection
+
 mysqli_close($conn);
 ?>
